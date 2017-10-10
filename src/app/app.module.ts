5a86b7c00a996454c8348b9293faf87e18052d41
Bekir Halvadzic <Bekir.Halvadzic@edpconsult.se>
2017-10-10 16:33:27 +0200

    är på routingen nu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,41 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }        from './app.component';
+import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent,
+      }
+    ]),
+    RouterModule.forRoot([
+      {
+        path: 'dashboard',
+        component: HeroesComponent,
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap: [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+selector: 'app-dashboard',
+templateUrl: './dashboard.component.html',
+})
+
+export class DashboardComponent implements OnInit {
+}
